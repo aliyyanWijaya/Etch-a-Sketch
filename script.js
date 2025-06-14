90777e350bf38e186ab17947be5764e9f158d1fa
aliyyanWijaya <muhammadqaliyyanwijaya@gmail.com>
2025-06-14 22:10:57 +1200

    add the script for adding div into page

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello world!");
+const containerDiv = document.createElement("div");
+containerDiv.classList.add("container");
+
+// add containerDiv into Body
+const mainBody = document.querySelector("body");
+mainBody.append(containerDiv);
+
+const gridDiv = document.createElement("div");
+gridDiv.classList.add("grid-div");
+containerDiv.append(gridDiv);
