d1d672261139f33575cf27c5e91555b8ff37f382
aliyyanWijaya <muhammadqaliyyanwijaya@gmail.com>
2025-06-15 10:35:31 +1200

    add the function for iterate div and change background color

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ containerDiv.classList.add("container");
 const mainBody = document.querySelector("body");
 mainBody.append(containerDiv);
 
-const gridDiv = document.createElement("div");
-gridDiv.classList.add("grid-div");
-containerDiv.append(gridDiv);
+function createGrid(grid) {
+    for (let i = 0; i < grid ** 2; i++) {
+        const gridDiv = document.createElement("div");
+        gridDiv.classList.add("grid-div");
+        containerDiv.append(gridDiv);
+    }
+}
+
+createGrid(16);
+
+const randNum = Math.floor(Math.random() * 256);
+const randColor = `rgb(${randNum}, ${randNum}, ${randNum})`;
+console.log(randColor);
+const gridDiv = document.querySelectorAll(".grid-div");
+
+gridDiv.forEach(div => {
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = `rgb(${randNum}, ${randNum}, ${randNum})`
+    })
+})
